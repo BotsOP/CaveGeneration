37d6032d576f0b92e85d25222bfd1d28d0006700
Denis Dvorkovyy <prutmaild@gmail.com>
2023-05-15 00:59:35 +0200

    kinda got vector field generation working

---
+++ Assets/Scripts/AgentAI/FollowVectorField.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowVectorField : MonoBehaviour
+{
+    public CaveVectorField caveVectorField;
+    private FollowVectorField followVectorField;
+    
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/CaveGeneration/CaveVectorField.cs
+++ Assets/Scripts/CaveGeneration/CaveVectorField.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -7,10 +8,9 @@ using UnityEngine.Rendering;
 public class CaveVectorField : MonoBehaviour
 {
     public Transform player;
-    private CaveManager caveManager;
-    private Queue<PathResult> results = new Queue<PathResult>();
-    private ComputeShader vectorFieldShader;
     public RenderTexture vectorField;
+    private CaveManager caveManager;
+    private ComputeShader vectorFieldShader;
     private int chunkSize => caveManager.chunkSize;
     private CaveChunk[,,] chunks => caveManager.chunks;
     private Vector3[] caveBounds => caveManager.caveBounds;
@@ -18,46 +18,52 @@ public class CaveVectorField : MonoBehaviour
     private int amountChunksVertical => caveManager.amountChunksVertical;
     private float isoLevel => caveManager.isoLevel;
     private ComputeBuffer countBuffer;
-    private ComputeBuffer pointsBufferPing;
-    private ComputeBuffer pointsBufferPong;
-    private bool shouldPing;
+    private ComputeBuffer appendPoints;
+    private ComputeBuffer seedPoints;
     private Vector3 threadGroupSize;
+    //private bool shouldPing;
 
-    private ComputeBuffer appendPoints
-    {
-        get
-        {
-            if (shouldPing)
-                return pointsBufferPing;
-            
-            return pointsBufferPong;
-        }
-    }
-    private ComputeBuffer consumePoints
-    {
-        get
-        {
-            if (shouldPing)
-                return pointsBufferPong;
-            
-            return pointsBufferPing;
-        }
-    }
+    // private ComputeBuffer appendPoints
+    // {
+    //     get
+    //     {
+    //         if (shouldPing)
+    //             return pointsBufferPing;
+    //         
+    //         return pointsBufferPong;
+    //     }
+    // }
+    // private ComputeBuffer consumePoints
+    // {
+    //     get
+    //     {
+    //         if (shouldPing)
+    //             return pointsBufferPong;
+    //         
+    //         return pointsBufferPing;
+    //     }
+    // }
 
     private void OnEnable()
     {
         caveManager = gameObject.GetComponent<CaveManager>();
         
         vectorFieldShader = Resources.Load<ComputeShader>("SDFVectorFieldPathfinder");
-        pointsBufferPing = new ComputeBuffer(1000, sizeof(int) * 3, ComputeBufferType.Structured);
-        pointsBufferPong = new ComputeBuffer(1000, sizeof(int) * 3, ComputeBufferType.Structured);
+        appendPoints = new ComputeBuffer(20000, sizeof(int) * 3, ComputeBufferType.Append);
+        seedPoints = new ComputeBuffer(20001, sizeof(int) * 3, ComputeBufferType.Structured);
+        appendPoints.SetData(new Int3[20000]);
+        appendPoints.SetCounterValue(0);
+        seedPoints.SetData(new Int3[20000]);
+        seedPoints.SetCounterValue(0);
         countBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Structured);
+        countBuffer.SetData(new int[1]);
         
         vectorFieldShader.GetKernelThreadGroupSizes(0, out uint threadGroupSizeX, out uint threadGroupSizeY, out uint threadGroupSizeZ);
         threadGroupSize = new Vector3(threadGroupSizeX, threadGroupSizeY, threadGroupSizeZ);
         
         vectorField = new RenderTexture(chunkSize, chunkSize, 0, RenderTextureFormat.ARGBFloat)
         {
+            filterMode = FilterMode.Point,
             dimension = TextureDimension.Tex3D,
             volumeDepth = chunkSize,
             enableRandomWrite = true,
@@ -66,17 +72,17 @@ public class CaveVectorField : MonoBehaviour
 
     private void OnDisable()
     {
-        pointsBufferPing.Dispose();
-        pointsBufferPing = null;
-        pointsBufferPong.Dispose();
-        pointsBufferPong = null;
+        appendPoints.Dispose();
+        appendPoints = null;
+        seedPoints.Dispose();
+        seedPoints = null;
         countBuffer.Dispose();
         countBuffer = null;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.N))
+        if (Input.GetKey(KeyCode.N))
         {
             GenerateVectorField(player.position);
         }
@@ -85,6 +91,10 @@ public class CaveVectorField : MonoBehaviour
     public RenderTexture GenerateVectorField(Vector3 _worldPos)
     {
         vectorField.Release();
+        appendPoints.SetData(new Int3[20000]);
+        appendPoints.SetCounterValue(0);
+        seedPoints.SetData(new Int3[20000]);
+        countBuffer.SetData(new int[1]);
 
         Vector3 chunkIndex = GetChunkIndex(_worldPos);
         CaveChunk chunk = chunks[(int)chunkIndex.x, (int)chunkIndex.y, (int)chunkIndex.z];
@@ -92,30 +102,38 @@ public class CaveVectorField : MonoBehaviour
         Vector3Int startPos = new Vector3Int((int)(_worldPos.x - chunk.position.x),
             (int)(_worldPos.y - chunk.position.y), (int)(_worldPos.z - chunk.position.z));
         Vector3Int[] startPosArray = { startPos };
-        appendPoints.SetData(startPosArray);
+        seedPoints.SetData(startPosArray);
+        int amountPointsToCheck = 1;
 
         int amountLoops = 0;
-        int amountPointsToCheck = 1;
         while (amountPointsToCheck > 0)
         {
             int threadGroupX = Mathf.CeilToInt(amountPointsToCheck / threadGroupSize.x);
             
-            appendPoints.SetCounterValue(0);
-
             vectorFieldShader.SetTexture(0, "vectorField", vectorField);
             vectorFieldShader.SetTexture(0, "noiseTex", chunk.noiseTex);
-            vectorFieldShader.SetBuffer(0, "consumePoints", consumePoints);
+            vectorFieldShader.SetBuffer(0, "seedPoints", seedPoints);
             vectorFieldShader.SetBuffer(0, "appendPoints", appendPoints);
             vectorFieldShader.SetBuffer(0, "counter", countBuffer);
             vectorFieldShader.SetFloat("isoLevel", isoLevel);
+            vectorFieldShader.SetInt("amountPoints", amountPointsToCheck);
             
-            vectorFieldShader.Dispatch(0, threadGroupX, 1, 1);
+            vectorFieldShader.Dispatch(0, threadGroupX, 6, 1);
 
             int[] countArray = new int[1];
             countBuffer.GetData(countArray);
             amountPointsToCheck = countArray[0];
             countBuffer.SetData(new int[1]);
 
+            Int3[] ints = new Int3[amountPointsToCheck];
+            appendPoints.GetData(ints);
+            ints = ints.Distinct().ToArray();
+            
+            appendPoints.SetCounterValue(0);
+            Vector3Int[] test = new Vector3Int[10];
+            seedPoints.GetData(test);
+            seedPoints.SetData(ints);
+            
             amountLoops++;
             if (amountLoops > 10000)
             {
@@ -134,37 +152,11 @@ public class CaveVectorField : MonoBehaviour
             new Vector3(amountChunksHorizontal, amountChunksVertical, amountChunksHorizontal));
     }
 
-    // public float GetTerrainValue(Vector3 _worldPos)
-    // {
-    //     //This doesnt work properly when its at the edge of a chunk but I dont care
-    //     float[] corners = new float[27];
-    //     
-    //     Vector3 chunkIndex = GetChunkIndex(_worldPos);
-    //     CaveChunk chunk = chunks[(int)chunkIndex.x, (int)chunkIndex.y, (int)chunkIndex.z];
-    //     Vector3 subChunkPos = new Vector3((int)_worldPos.x - chunk.position.x, (int)_worldPos.y - chunk.position.y, (int)_worldPos.z - chunk.position.z);
-    //     
-    //     getSDFInfoShader.SetTexture(4, "noiseTex", chunk.noiseTex);
-    //     getSDFInfoShader.SetBuffer(4, "neighbours", neighboursBuffer);
-    //     getSDFInfoShader.SetVector("currentPos", subChunkPos);
-    //
-    //     getSDFInfoShader.Dispatch(4, 1, 1, 1);
-    //
-    //     neighboursBuffer.GetData(corners);
-    //     
-    //     Vector3 posInsideSubChunk = _worldPos - chunk.position - subChunkPos;
-    //     posInsideSubChunk = new Vector3(Mathf.Abs(posInsideSubChunk.x), Mathf.Abs(posInsideSubChunk.y), Mathf.Abs(posInsideSubChunk.z));
-    //     
-    //     float c00 = corners[0] * (1 - posInsideSubChunk.x) + corners[1] * posInsideSubChunk.x;
-    //     float c01 = corners[2] * (1 - posInsideSubChunk.x) + corners[3] * posInsideSubChunk.x;
-    //     float c10 = corners[4] * (1 - posInsideSubChunk.x) + corners[5] * posInsideSubChunk.x;
-    //     float c11 = corners[6] * (1 - posInsideSubChunk.x) + corners[7] * posInsideSubChunk.x;
-    //
-    //     float c0 = c00 * (1 - posInsideSubChunk.y) + c01 * posInsideSubChunk.y;
-    //     float c1 = c10 * (1 - posInsideSubChunk.y) + c11 * posInsideSubChunk.y;
-    //
-    //     float result = c0 * (1 - posInsideSubChunk.z) + c1 * posInsideSubChunk.z;
-    //     
-    //     return result;
-    // }
+    struct Int3
+    {
+        public int x;
+        public int y;
+        public int z;
+    }
 }
 
